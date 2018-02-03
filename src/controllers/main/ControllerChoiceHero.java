77e9618c2e19568d5281811989e9f7e9ced47a02
StarKRE <ardar.igor@mail.ru>
2018-02-03 12:44:38 +0700

    03.02.2018 WORKING WITH HERO CHOICE CONTROLLER: developed ControllerProfile.

--- src/controllers/main/ControllerChoiceHero.java
+++ src/controllers/main/ControllerChoiceHero.java
@@ -54,8 +54,6 @@ public class ControllerChoiceHero implements Initializable, Controller {
 
 
 
-
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         appearance();
